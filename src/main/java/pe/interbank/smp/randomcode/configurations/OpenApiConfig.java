c6c671bb48fa3fd573a54345944cd34dedce22bf
Álvaro Díaz <133562578+alvarodiaz-dev@users.noreply.github.com>
2026-04-29 22:35:23 -0500

    Add OpenApiConfig.java

---
+++ src/main/java/pe/interbank/smp/randomcode/configurations/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package pe.interbank.smp.randomcode.configurations;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Random Code Generation Service API")
+                        .description("Microservicio para generación de códigos aleatorios de 4 dígitos")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("API Support")
+                                .email("support@example.com"))
+                        .license(new License()
+                                .name("MIT")
+                                .url("https://opensource.org/licenses/MIT")));
+    }
+}
